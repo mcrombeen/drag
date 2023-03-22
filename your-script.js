bcb6e9a1bfb775c70d4d625574540ced342798cf
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-03-22 18:06:20 +0100

    Update your-script.js

--- your-script.js
+++ your-script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js/addons/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
