7f3264f351488a0023d3661b511b9b4df616c129
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-04-08 13:41:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,126 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const fab = document.getElementById('fab');
-    const imagePicker = document.getElementById('image-picker');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const fab = document.getElementById('fab');
+  const imagePicker = document.getElementById('image-picker');
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    let image = null;
-    let isDragging = false;
-    let selectedImage = null;
+  let image = null;
+  let isDragging = false;
+  let selectedImage = null;
 
-    function loadImage(file) {
-        const img = new Image();
-        const reader = new FileReader();
-        reader.onload = (event) => {
-            img.src = event.target.result;
-        };
-        reader.readAsDataURL(file);
+  function loadImage(file) {
+    const img = new Image();
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
 
-        img.onload = () => {
-            const imgX = Math.floor(Math.random() * (canvas.width - img.width));
-            const imgY = Math.floor(Math.random() * (canvas.height - img.height));
+    img.onload = () => {
+      const imgX = Math.floor(Math.random() * (canvas.width - img.width));
+      const imgY = Math.floor(Math.random() * (canvas.height - img.height));
 
-            image = { img, imgX, imgY, width: img.width, height: img.height };
-            drawImage();
-        };
+      image = { img, imgX, imgY, width: img.width, height: img.height };
+      drawImage();
+    };
+  }
+
+  function drawImage() {
+    if (image) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image.img, image.imgX, image.imgY, image.width, image.height);
     }
+  }
 
-    function drawImage() {
-        if (image) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(image.img, image.imgX, image.imgY, image.width, image.height);
-        }
+  function highlightImage(ctx, image) {
+    const { img, imgX, imgY, width, height } = image;
+    const padding = 5;
+    const highlightColor = 'rgba(0, 255, 0, 0.5)';
+
+    ctx.fillStyle = highlightColor;
+    ctx.fillRect(imgX - padding, imgY - padding, width + 2 * padding, height + 2 * padding);
+    ctx.drawImage(img, imgX, imgY, width, height);
+  }
+
+  fab.addEventListener('click', () => {
+    imagePicker.click();
+  });
+
+  imagePicker.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      loadImage(file);
     }
+  });
 
-    function highlightImage(ctx, image) {
-        const { img, imgX, imgY, width, height } = image;
-        const padding = 5;
-        const highlightColor = 'rgba(0, 255, 0, 0.5)';
+  canvas.addEventListener('mousedown', (event) => {
+    if (!image) return;
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
 
-        ctx.fillStyle = highlightColor;
-        ctx.fillRect(imgX - padding, imgY - padding, width + 2 * padding, height + 2 * padding);
-        ctx.drawImage(img, imgX, imgY, width, height);
+    if (mouseX >= image.imgX && mouseX <= image.imgX + image.width && mouseY >= image.imgY && mouseY <= image.imgY + image.height) {
+      isDragging = true;
+
+      // Highlight the selected image
+      if (selectedImage !== image) {
+        selectedImage = image;
+        drawImage();
+        highlightImage(ctx, selectedImage);
+      }
+    } else {
+      // Unselect the image
+      selectedImage = null;
+      drawImage();
     }
+  });
 
-    fab.addEventListener('click', () => {
-        imagePicker.click();
-    });
+  canvas.addEventListener('mousemove', (event) => {
+    if (isDragging && image) {
+      image.imgX = event.clientX - image.width / 2;
+      image.imgY = event.clientY - image.height / 2;
+      drawImage();
+      if (selectedImage) {
+        highlightImage(ctx, selectedImage);
+      }
+    }
+  });
 
-    imagePicker.addEventListener('change', (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            loadImage(file);
+  canvas.addEventListener('mouseup', () => {
+    isDragging = false;
+  });
+
+  canvas.addEventListener('wheel', (event) => {
+    if (selectedImage) {
+        const delta = Math.sign(event.deltaY);
+        const step = 16;
+        const minWidthHeight = 16;
+        const maxWidthHeight = 128;
+
+        const newWidth = selectedImage.width - delta * step;
+        const newHeight = selectedImage.height - delta * step;
+
+        if (newWidth >= minWidthHeight && newWidth <= maxWidthHeight && newHeight >= minWidthHeight && newHeight <= maxWidthHeight) {
+            selectedImage.width = newWidth;
+            selectedImage.height = newHeight;
         }
-    });
 
-    canvas.addEventListener('mousedown', (event) => {
-        if (!image) return;
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
+        drawImage();
+        highlightImage(ctx, selectedImage);
+    }
+});
 
-        if (mouseX >= image.imgX && mouseX <= image.imgX + image.width && mouseY >= image.imgY && mouseY <= image.imgY + image.height) {
-            isDragging = true;
+canvas.addEventListener('gesturestart', (event) => {
+    event.preventDefault();
+});
 
-            // Highlight the selected image
-            if (selectedImage !== image) {
-                selectedImage = image;
-                drawImage();
-                highlightImage(ctx, selectedImage);
-            }
-        } else {
-            // Unselect the image
-            selectedImage = null;
-            drawImage();
-        }
-    });
-
-    canvas.addEventListener('mousemove', (event) => {
-        if (isDragging && image) {
-            image.imgX = event.clientX - image.width / 2;
-            image.imgY = event.clientY - image.height / 2;
-            drawImage();
-            if (selectedImage) {
-                highlightImage(ctx, selectedImage);
-            }
-        }
-    });
-
-    canvas.addEventListener('mouseup', () => {
-        isDragging = false;
-    });
-
-    canvas.addEventListener('wheel', (event) => {
+canvas.addEventListener('gesturechange', (event) => {
+    event.preventDefault();
+    if (event.scale !== 1) {
         if (selectedImage) {
-            const delta = Math.sign(event.deltaY);
+            const delta = (event.scale > 1 ? 1 : -1) * 8;
             const step = 16;
             const minWidthHeight = 16;
             const maxWidthHeight = 128;
@@ -110,4 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
             drawImage();
             highlightImage(ctx, selectedImage);
         }
-    })
+    }
+});
+
+});
